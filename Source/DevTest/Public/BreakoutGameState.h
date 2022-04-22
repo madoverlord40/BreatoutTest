ba785ee7ebff6bf2f099c581631e540a397edc50
madoverlord40 <jamesh39@outlook.com>
2022-04-21 19:02:04 -0700

    initial C++ stubs

---
+++ Source/DevTest/Public/BreakoutGameState.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameStateBase.h"
+#include "BreakoutGameState.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class DEVTEST_API ABreakoutGameState : public AGameStateBase
+{
+	GENERATED_BODY()
+	
+};
---
+++ Source/DevTest/Public/BreakoutPlayerController.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/PlayerController.h"
+#include "BreakoutPlayerController.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class DEVTEST_API ABreakoutPlayerController : public APlayerController
+{
+	GENERATED_BODY()
+	
+};
